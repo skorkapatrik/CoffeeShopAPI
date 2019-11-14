b20be0a073336cf84fb9921a93136b2ef0c0200b
Egri Sándor <alexander.egri@gmail.com>
2019-11-15 00:01:40 +0100

    CoffeShop Desktop 0.0.1

---
+++ CoffeShopDesktop/CoffeShopDesktop/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoffeShopDesktop
+{
+    public partial class Form1 : Form
+    {
+        private string url = "http://localhost:8080/";
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int value;
+
+            if (int.TryParse(textBox1.Text, out value))
+            {
+                string urlParameters = $"manufacturer/{value}";
+                var response = APICall.RunAsync<Manufacturer>(url, urlParameters).GetAwaiter().GetResult();
+
+                textBox2.Text = response.Name;
+                textBox3.Text = response.Country;
+                textBox4.Text = response.Address;
+                textBox5.Text = response.Phone;
+
+
+            }
+            else
+            {
+                string urlParameters = $"manufacturer/";
+                var response = APICall.RunAsync<List<Manufacturer>>(url, urlParameters).GetAwaiter().GetResult();
+                
+            }
+        }
+
+    }
+}
---
+++ CoffeShopDesktop/CoffeShopDesktop/Manufacturer.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeShopDesktop
+{
+    class Manufacturer
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("country")]
+        public string Country { get; set; }
+        [JsonProperty("address")]
+        public string Address { get; set; }
+        [JsonProperty("phone")]
+        public string Phone { get; set; }
+
+    }
+}
